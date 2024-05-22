6885e8f600a79d838e11f9d8a7a7a2200ac4f6f8
Priscila Romano <161603421+PriRomano@users.noreply.github.com>
2024-05-22 12:37:15 -0300

    commit fim da aula

---
+++ src/Jolteon.java
@@ -0,0 +1,14 @@
+public class Jolteon extends Eevee {
+    @Override
+    public String ataque () {
+        return "Trovoada - thunder-Shock";
+    }
+    @Override
+    public String defesa () {
+        return "Carga - Wild Charge";
+    }
+    @Override
+    public String especial () {
+        return "Raio - Thunder";
+    }
+}
