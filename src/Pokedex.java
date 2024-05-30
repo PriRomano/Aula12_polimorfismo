407f992fd2737f0f307cd6c64758c84e7e7992bf
Priscila Romano <161603421+PriRomano@users.noreply.github.com>
2024-05-29 23:13:24 -0300

    prints na pokedex

--- src/Pokedex.java
+++ src/Pokedex.java
@@ -13,7 +13,7 @@ public class Pokedex {
 
         //OK Implementar as outras classes - falta ver os nomes dos ataques
         //OK implementar getters e setters
-        //OK implementar o construtor, com os valores das variáveis
+        // implementar o construtor, com os valores das variáveis
         // implementar o método imprimir, exibir os valores das variáveis e os métodos
         // exibir o resultado na classe Pokedex
         // mandar link do github na atividade
@@ -33,9 +33,70 @@ public class Pokedex {
         System.out.println("Defesa: " + j.defesa());
         System.out.println("Especial: " + j.especial());
 
+        // Pula linha
+        System.out.println(" ");
+
+        // Print do Espeon 
+        System.out.println("Eu sou: Espeon");
+        System.out.println("Ataque: " + es.ataque());
+        System.out.println("Defesa: " + es.defesa());
+        System.out.println("Especial: " + es.especial());
+
+        // Pula linha
+        System.out.println(" ");
+
+        // Print do Flareon 
+        System.out.println("Eu sou: Flareon");
+        System.out.println("Ataque: " + f.ataque());
+        System.out.println("Defesa: " + f.defesa());
+        System.out.println("Especial: " + f.especial());
+
+        // Pula linha
+        System.out.println(" ");
+
+        // Print do Glaceon 
+        System.out.println("Eu sou: Glaceon");
+        System.out.println("Ataque: " + g.ataque());
+        System.out.println("Defesa: " + g.defesa());
+        System.out.println("Especial: " + g.especial());
+
+        // Pula linha
+        System.out.println(" ");
+
+        // Print do Leafeon 
+        System.out.println("Eu sou: Leafeon");
+        System.out.println("Ataque: " + l.ataque());
+        System.out.println("Defesa: " + l.defesa());
+        System.out.println("Especial: " + l.especial());
+
+        // Pula linha
+        System.out.println(" ");
+
+        // Print do Sylveon 
+        System.out.println("Eu sou: Sylveon");
+        System.out.println("Ataque: " + s.ataque());
+        System.out.println("Defesa: " + s.defesa());
+        System.out.println("Especial: " + s.especial());
+
+        // Pula linha
+        System.out.println(" ");
+
+        // Print do Umbreon 
+        System.out.println("Eu sou: Umbreon");
+        System.out.println("Ataque: " + u.ataque());
+        System.out.println("Defesa: " + u.defesa());
+        System.out.println("Especial: " + u.especial());
+
+        // Pula linha
+        System.out.println(" ");
+
+        // Print do Vaporeon 
+        System.out.println("Eu sou: Vaporeon");
+        System.out.println("Ataque: " + v.ataque());
+        System.out.println("Defesa: " + v.defesa());
+        System.out.println("Especial: " + v.especial());
+
         
-
-
         //e=j; -    P deixar os valores da Eeve = os do jolteon
         //System.out.println ("Eu sou: Eevee");
         //System.out.println("Ataque: " + e.ataque());
